9e33ef54e348e582a932e6b9899b5600c63574b1
Nuray <210103331@stu.sdu.edu.kz>
2023-11-29 11:33:17 +0600

    topAppBar main

--- app/src/main/java/com/example/amphibians/MainActivity.kt
+++ app/src/main/java/com/example/amphibians/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.amphibians.ui.theme.AmphibiansApp
 import com.example.amphibians.ui.theme.AmphibiansTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AmphibiansApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AmphibiansTheme {
-        Greeting("Android")
-    }
-}
