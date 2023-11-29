d47a0f2183dd3c5c9c1df98de1ebdc9cd5c63a33
Nuray <210103331@stu.sdu.edu.kz>
2023-11-29 11:53:48 +0600

    model and network

---
+++ app/src/main/java/com/example/amphibians/ui/model/Amphibian.kt
@@ -0,0 +1,14 @@
+package com.example.amphibians.ui.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+
+@Serializable
+data class Amphibian(
+    val name: String,
+    val type: String,
+    val description: String,
+    @SerialName("img_src") val imgSrc: String
+)
--- app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
@@ -1,8 +1,18 @@
 package com.example.amphibians.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen(modfier : Modifier = Modifier){
+    LazyColumn(
+
+
+        verticalArrangement = Arrangement.spacedBy(24.dp)){
+
+    }
 
 }
